4de9559b88ce450d3f33b59650a2c2ab6c67e6d6
abdlatif-nabgha <abdelatif.nabgha06@gmail.com>
2026-07-09 17:04:16 +0100

    initial microservices events setup

---
+++ libs/shared/src/index.ts
@@ -0,0 +1,13 @@
+export * from './shared.module';
+export * from './shared.service';
+
+
+// Constants
+export * from './constants';
+
+// Interfaces
+export * from './interfaces';
+
+// Utils
+export * from './utils';
+
